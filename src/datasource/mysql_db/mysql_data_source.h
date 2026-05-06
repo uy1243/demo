c7fe5df4b544562a4e15f897849bae808f2d3e3b
naiyu <4700289@163.com>
2026-05-07 07:00:14 +0800

    add Sql

--- src/datasource/mysql_db/mysql_data_source.h
+++ src/datasource/mysql_db/mysql_data_source.h
@@ -3,19 +3,25 @@
 #include "../market_types.h"
 #include <string>
 #include <vector>
-#include <mysqlx/xdevapi.h>
-#include <map> // 用于按时间排序数据
-
-using namespace mysqlx;
+#include <map>
+#include <mysql_connection.h>
+#include <mysql_driver.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/resultset.h>
+#include <cppconn/exception.h>
 
 class MysqlDataSource : public IDataSource {
 public:
-    MysqlDataSource(const std::string& host, int port, const std::string& user, const std::string& password, const std::string& database);
+    MysqlDataSource(const std::string& host, int port,
+        const std::string& user, const std::string& password,
+        const std::string& database);
+
+    ~MysqlDataSource(); // 添加析构函数以清理资源
 
     std::string getName() const override { return "MYSQL"; }
-    std::vector<TickData> fetchQuotes(const std::string& instrument) override; // 获取指定合约的历史数据
+    std::vector<TickData> fetchQuotes(const std::string& instrument) override;
 
-    // 专门用于回测的新方法：获取指定时间段内的所有数据
     std::multimap<std::string, TickData> fetchHistoricalData(
         const std::string& instrument,
         const std::string& start_time,
@@ -29,5 +35,9 @@ private:
     std::string password_;
     std::string database_;
 
-    Session getSession();
+    sql::mysql::MySQL_Driver* driver_;      // MySQL 驱动对象
+    sql::Connection* connection_;           // 数据库连接对象
+
+    void connect();                         // 建立连接
+    void disconnect();                      // 断开连接
 };
